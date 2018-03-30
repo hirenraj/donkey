e7e663cc0b000289459c70c959bd03926c6875d9
EC2 Default User <ec2-user@ip-172-31-26-31.ec2.internal>
2018-03-30 20:39:03 +0000

    fixed gpu files

--- sagetrain/sagetrain.py
+++ sagetrain/sagetrain.py
@@ -4,15 +4,13 @@ from sagemaker import get_execution_role
 
 sagemaker_session = sage.Session()
 
-#role = get_execution_role()
+role = get_execution_role()
 
-role='arn:aws:iam::271275436050:role/service-role/AmazonSageMaker-ExecutionRole-20171206T112881'
-
-inputs = "s3://sagemaker-us-east-1-271275436050/data/racecar/"
-image="271275436050.dkr.ecr.us-east-1.amazonaws.com/deepimage"
+inputs = "s3://sagemaker-us-east-1-xxxx/data/racecar/"
+image="writeyourimagename"
 algo = sage.estimator.Estimator(image,
                        role, 1, 'ml.p2.xlarge',
-                       output_path="s3://{}/output".format("sagemaker-keji"),
+                       output_path="s3://{}/output".format(sagemaker_session.default_bucket()),
                        sagemaker_session=sagemaker_session)
 
 algo.fit(inputs)
